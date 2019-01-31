d5710d9e2dc0fc3d0888416ee48f0f2df219120f
phnguyentri <phnguyentri@gmail.com>
2019-01-31 11:28:30 +0700

    Update code for edit form  Marvelic_Job

--- app/code/Marvelic/Job/view/adminhtml/web/js/custom.js
+++ app/code/Marvelic/Job/view/adminhtml/web/js/custom.js
@@ -6,32 +6,56 @@ require(['jquery', 'jquery/ui', 'uiRegistry'], function($, ui, reg){
     reg.get([
             prefixForm + '.source.data_export_source',
             prefixForm + '.date.date_config',
+            prefixForm + '.date',
+            prefixForm + '.date.type_time',
+            prefixForm + '.date.period',
+            prefixForm + '.source',
+            prefixForm + '.source.export_source',
             prefixForm + '.source.file_path',
             prefixForm + '.source.host',
             prefixForm + '.source.port',
             prefixForm + '.source.username',
             prefixForm + '.source.password'
-            ], function (dataExport, dataDate, filePath, host, port, username, password){
+            ], function (dataExport,
+                         dataDate,
+                         date,
+                         dateTypeTime,
+                         datePeriod,
+                         source,
+                         typeExport,
+                         filePath,
+                         host,
+                         port,
+                         username,
+                         password){
 
-        if(dataDate.value() === '' || dataExport.value() === ''){
+        if(dataDate.value() !== '' || dataExport.value() !== ''){
             let jsonExport  = JSON.parse(dataExport.value());
             let jsonDate    = JSON.parse(dataDate.value());
 
-            reg.get(prefixForm + '.date.type_time').value(jsonDate.type_time);
-            reg.get(prefixForm + '.date.period').value(jsonDate.period);
+            console.log(jsonExport);
 
-            reg.get(prefixForm + '.source').toggleOpened();
-            reg.get(prefixForm + '.source.export_source').value(jsonExport.type);
+            date.toggleOpened();
+            dateTypeTime.value(jsonDate.type_time);
+            datePeriod.value(jsonDate.period);
+
+            source.toggleOpened();
 
             let intervalHideInputDone = setInterval(function(){
                 console.log(filePath.visible());
                 if (filePath.visible() === false
-                    || host.visible() === false
-                    || port.visible() === false
-                    || username.visible() === false
-                    || password.visible() === false
+                    && host.visible() === false
+                    && port.visible() === false
+                    && username.visible() === false
+                    && password.visible() === false
                 ){
-                    displayExportSource(reg.get(prefixForm + '.source.export_source').value());
+                    typeExport.value(jsonExport.type);
+                    displayExportSource(typeExport.value());
+                    filePath.value(jsonExport.file_path);
+                    host.value(jsonExport.host);
+                    port.value(jsonExport.port);
+                    username.value(jsonExport.username);
+                    password.value(jsonExport.password);
                     clearInterval(intervalHideInputDone);
                 }
             }, 200);
@@ -79,34 +103,3 @@ require(['jquery', 'jquery/ui', 'uiRegistry'], function($, ui, reg){
     }
 
 });
-// notification_area.notification_area_data_source
-// VM10451:2 marvelic_job_export_form.export_form_data_source
-// VM10451:2 notification_area.notification_area_data_source_storage
-// VM10451:2 notification_area.notification_area
-// VM10451:2 notification_area.notification_area.modalContainer
-// VM10451:2 notification_area.notification_area.modalContainer.modal
-// VM10451:2 notification_area.notification_area.columns.created_at
-// VM10451:2 notification_area.notification_area.columns.actions
-// VM10451:2 notification_area.notification_area.columns.dismiss
-// VM10451:2 marvelic_job_export_form.marvelic_job_export_form.general
-// VM10451:2 marvelic_job_export_form.marvelic_job_export_form.date
-// VM10451:2 marvelic_job_export_form.marvelic_job_export_form.source
-// VM10451:2 notification_area.notification_area.columns
-// VM10451:2 notification_area.notification_area.modalContainer.modal.insertBulk
-// VM10451:2 notification_area.notification_area.columns_dnd
-// VM10451:2 marvelic_job_export_form.marvelic_job_export_form
-// VM10451:2 marvelic_job_export_form.marvelic_job_export_form.general.title
-// VM10451:2 marvelic_job_export_form.marvelic_job_export_form.date.period
-// VM10451:2 marvelic_job_export_form.marvelic_job_export_form.date.date_config
-// VM10451:2 marvelic_job_export_form.marvelic_job_export_form.source.file_path
-// VM10451:2 marvelic_job_export_form.marvelic_job_export_form.source.host
-// VM10451:2 marvelic_job_export_form.marvelic_job_export_form.source.port
-// VM10451:2 marvelic_job_export_form.marvelic_job_export_form.source.username
-// VM10451:2 marvelic_job_export_form.marvelic_job_export_form.source.password
-// VM10451:2 marvelic_job_export_form.marvelic_job_export_form.source.data_export_source
-// VM10451:2 marvelic_job_export_form.marvelic_job_export_form.general.is_active
-// VM10451:2 marvelic_job_export_form.marvelic_job_export_form.general.cron
-// VM10451:2 marvelic_job_export_form.marvelic_job_export_form.general.entity
-// VM10451:2 marvelic_job_export_form.marvelic_job_export_form.date.type_time
-// VM10451:2 marvelic_job_export_form.marvelic_job_export_form.general.frequency
-// VM10451:2 marvelic_job_export_form.marvelic_job_export_form.source.export_source
